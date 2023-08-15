257e1ecc5e0cc438a7f8b3a654af5fdf965fd077
Алекс Ш <slayer1649@gmail.com>
2023-08-15 14:10:59 +0300

    update

--- git_repo_manager.sh
+++ git_repo_manager.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+GREEN="\e[32m"
+CYAN="\e[36m"
+YELLOW="\e[33m"
+RED="\e[31m"
+RESET="\e[0m"
+
+print_error() {
+    echo -e "${RED}Error: $1${RESET}"
+}
+
 # Function to create a new Git repository
 create_repository() {
     local repo_name="$1"
-    mkdir "$repo_name"
-    cd "$repo_name" || exit
-    git init
-    echo "Created and initialized repository: $repo_name"
-    cd ..
+    if [ -d "$repo_name" ]; then
+        print_error "Repository '$repo_name' already exists."
+    else
+        mkdir "$repo_name" && cd "$repo_name" || exit
+        git init
+        echo -e "${GREEN}Created and initialized repository: $repo_name${RESET}"
+        cd ..
+    fi
 }
 
 # Function to clone a Git repository
@@ -18,30 +31,46 @@ clone_repository() {
 
 # Function to list all repositories
 list_repositories() {
-    ls -d */
+    local repositories=($(ls -d */))
+    if [ ${#repositories[@]} -eq 0 ]; then
+        echo -e "${YELLOW}No repositories found.${RESET}"
+    else
+        echo -e "${CYAN}List of repositories:${RESET}"
+        for repo in "${repositories[@]}"; do
+            echo "- $repo"
+        done
+    fi
 }
 
 # Function to delete a repository
 delete_repository() {
     local repo_name="$1"
-    read -p "Are you sure you want to delete $repo_name? (y/n): " confirm
-    if [[ $confirm == "y" ]]; then
-        rm -rf "$repo_name"
-        echo "Deleted repository: $repo_name"
+    if [ -d "$repo_name" ]; then
+        read -p "Are you sure you want to delete '$repo_name'? (y/n): " confirm
+        if [[ $confirm == "y" ]]; then
+            rm -rf "$repo_name"
+            echo -e "${GREEN}Deleted repository: $repo_name${RESET}"
+        else
+            echo "Deletion canceled."
+        fi
     else
-        echo "Deletion canceled."
+        print_error "Repository '$repo_name' not found."
     fi
 }
 
 # Function to display repository information
 repository_info() {
     local repo_name="$1"
-    cd "$repo_name" || exit
-    echo "Repository Information: $repo_name"
-    echo "========================="
-    git remote -v
-    git branch -a
-    cd ..
+    if [ -d "$repo_name" ]; then
+        cd "$repo_name" || exit
+        echo -e "${CYAN}Repository Information: $repo_name${RESET}"
+        echo "========================="
+        git remote -v
+        git branch -a
+        cd ..
+    else
+        print_error "Repository '$repo_name' not found."
+    fi
 }
 
 # Main menu
@@ -60,10 +89,12 @@ while true; do
         1)
             read -rp "Enter the name of the new repository: " repo_name
             create_repository "$repo_name"
+            read -rp "Press Enter to continue..."
             ;;
         2)
             read -rp "Enter the URL of the repository to clone: " repo_url
             clone_repository "$repo_url"
+            read -rp "Press Enter to continue..."
             ;;
         3)
             list_repositories
@@ -84,7 +115,7 @@ while true; do
             exit 0
             ;;
         *)
-            echo "Invalid choice. Please select a valid option."
+            print_error "Invalid choice. Please select a valid option."
             read -rp "Press Enter to continue..."
             ;;
     esac
