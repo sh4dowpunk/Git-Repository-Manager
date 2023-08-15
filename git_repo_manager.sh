4508f774c2c534c6d68835a9d5faf2ae5e7b26e1
Алекс Ш <slayer1649@gmail.com>
2023-08-15 14:09:01 +0300

    update

--- git_repo_manager.sh
+++ git_repo_manager.sh
@@ -21,6 +21,29 @@ list_repositories() {
     ls -d */
 }
 
+# Function to delete a repository
+delete_repository() {
+    local repo_name="$1"
+    read -p "Are you sure you want to delete $repo_name? (y/n): " confirm
+    if [[ $confirm == "y" ]]; then
+        rm -rf "$repo_name"
+        echo "Deleted repository: $repo_name"
+    else
+        echo "Deletion canceled."
+    fi
+}
+
+# Function to display repository information
+repository_info() {
+    local repo_name="$1"
+    cd "$repo_name" || exit
+    echo "Repository Information: $repo_name"
+    echo "========================="
+    git remote -v
+    git branch -a
+    cd ..
+}
+
 # Main menu
 while true; do
     clear
@@ -28,7 +51,9 @@ while true; do
     echo "1. Create a new repository"
     echo "2. Clone a repository"
     echo "3. List repositories"
-    echo "4. Exit"
+    echo "4. Delete a repository"
+    echo "5. Repository information"
+    echo "6. Exit"
     read -rp "Enter your choice: " choice
 
     case $choice in
@@ -45,6 +70,16 @@ while true; do
             read -rp "Press Enter to continue..."
             ;;
         4)
+            read -rp "Enter the name of the repository to delete: " repo_to_delete
+            delete_repository "$repo_to_delete"
+            read -rp "Press Enter to continue..."
+            ;;
+        5)
+            read -rp "Enter the name of the repository: " repo_to_show
+            repository_info "$repo_to_show"
+            read -rp "Press Enter to continue..."
+            ;;
+        6)
             echo "Exiting..."
             exit 0
             ;;
